82803678b1071ff1a8d055b68e601f9113868d28
Suprio-Das <priyamd507@gmail.com>
2025-04-12 19:09:55 +0600

    firebase init modified due security

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyCFaWBIGSs5NF_DY3ifXVXr5JLbdpuxfkY",
+    authDomain: "pccc-identity-auth.firebaseapp.com",
+    projectId: "pccc-identity-auth",
+    storageBucket: "pccc-identity-auth.firebasestorage.app",
+    messagingSenderId: "266004199284",
+    appId: "1:266004199284:web:72ea6f979af7863f111e21"
+};
+
+const app = initializeApp(firebaseConfig);
