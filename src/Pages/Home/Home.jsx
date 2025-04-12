f54aeb0fe568ef764d6817a1261416b78d586aa5
Suprio-Das <priyamd507@gmail.com>
2025-04-12 18:06:26 +0600

    PCCC logo added to the home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import Logo from '../../assets/PCIU-Computer-Club-Logo.png'
 
 const Home = () => {
     return (
-        <div className='min-h-[calc(100vh-70px)] flex items-center justify-center'>
-            <h1 className='lg:text-5xl text-xl font-semibold text-center text-blue-500'>PCCC Identity Portal</h1>
+        <div className='min-h-[calc(100vh-80px)] flex items-center justify-center'>
+            <div>
+                <div className='flex justify-center'>
+                    <img src={Logo} alt="Logo" className='w-38 my-3' />
+                </div>
+                <h1 className='lg:text-5xl text-xl font-semibold text-center text-blue-500'>PCCC Identity Portal</h1>
+            </div>
         </div>
     );
 };
