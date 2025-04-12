8246259dc3afb14ca5aa5f39b714095e97d206cb
Suprio-Das <priyamd507@gmail.com>
2025-04-12 16:43:34 +0600

    Home component created and added to the main layout

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home";
 
 const router = createBrowserRouter([
     {
@@ -8,7 +9,7 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element: <h1>This is Home.</h1>
+                element: <Home></Home>
             }
         ]
     }
