6232bf45331be955f5db254853aad1139fa5a419
Suprio-Das <priyamd507@gmail.com>
2025-04-12 18:02:11 +0600

    Logo added to the navbar

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import PCIU_Computer_Club_Logo from '../../assets/PCIU-Computer-Club-Logo.png';
 import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="flex-1">
-                <Link to='/' className="btn btn-ghost text-xl">PCCC Identity Portal</Link>
+                <div className='flex items-center gap-2'>
+                    <img src={PCIU_Computer_Club_Logo} alt="PCCC_Identity_Logo" className='w-[60px]' />
+                    <Link to='/' className='text-xl font-semibold'>PCCC Identity Portal</Link>
+                </div>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
