c7c7a72c66768b09db2481dbdf0a9dac960c9a83
Suprio-Das <priyamd507@gmail.com>
2025-04-12 16:58:30 +0600

    Navbar created and added to the mainlayout

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Pages/Shared/Navbar';
 
 const MainLayout = () => {
     return (
         <>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </>
     );
---
+++ src/Pages/Shared/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="flex-1">
+                <Link to='/' className="btn btn-ghost text-xl">PCCC Identity Portal</Link>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><NavLink to='/'>Home</NavLink></li>
+                    <li><NavLink>Login</NavLink></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
