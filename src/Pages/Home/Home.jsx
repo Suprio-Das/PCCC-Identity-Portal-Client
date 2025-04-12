9d9aac924cee44cf3bf279a7575cc370466f66b7
Suprio-Das <priyamd507@gmail.com>
2025-04-12 17:44:38 +0600

    home layout modified

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home.</h1>
+        <div className='min-h-[calc(100vh-70px)] flex items-center justify-center'>
+            <h1 className='lg:text-5xl text-xl font-semibold text-center text-blue-500'>PCCC Identity Portal</h1>
         </div>
     );
 };
