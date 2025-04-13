51943cd312464d4c7a65655d968ef4ec7fee5d37
Suprio-Das <priyamd507@gmail.com>
2025-04-13 23:27:15 +0600

    user and loading state called in PrivateRoute using context

--- src/Router/PrivateRoutes.jsx
+++ src/Router/PrivateRoutes.jsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
+import AuthContext from '../Context/AuthContext';
 
 const PrivateRoutes = () => {
+    const { user, loading } = useContext(AuthContext);
     return (
         <Navigate to='/login'></Navigate>
     );
