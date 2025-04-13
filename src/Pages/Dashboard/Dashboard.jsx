fd7a97e2bdf6886ce761e5125b426948aee893ee
Suprio-Das <priyamd507@gmail.com>
2025-04-14 00:48:44 +0600

    Dashboard title added

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div>
-            This is dashboard.
+            <h1 className='lg:text-3xl font-semibold text-blue-500 text-center my-3'><blockquote>PCCC Member Identity Zone!</blockquote> </h1>
         </div>
     );
 };
