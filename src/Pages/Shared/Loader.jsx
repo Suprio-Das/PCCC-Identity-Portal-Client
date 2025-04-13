4f638ee19a827e91ed78a323e3ee8c74da109212
Suprio-Das <priyamd507@gmail.com>
2025-04-13 23:52:35 +0600

    Lottie loader designed

--- src/Pages/Shared/Loader.jsx
+++ src/Pages/Shared/Loader.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import Lottie from 'lottie-react';
+import Loader_Lottie from '../../assets/loader_lottie.json';
 
 const Loader = () => {
     return (
-        <div>
-
+        <div className='min-h-[calc(100vh-70px)] flex item-center justify-center'>
+            <Lottie animationData={Loader_Lottie} className='w-62'></Lottie>
         </div>
     );
 };
