60e66750a5886005d9bbf5815e8ebfe07d498ed4
Suprio-Das <priyamd507@gmail.com>
2025-04-13 23:09:26 +0600

    home info text changed according to the login/logout

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/PCIU-Computer-Club-Logo.png'
+import { useContext } from 'react';
+import AuthContext from '../../Context/AuthContext';
 
 const Home = () => {
+    const { user } = useContext(AuthContext);
     return (
         <div className='min-h-[calc(100vh-80px)] flex items-center justify-center'>
             <div>
@@ -9,7 +12,11 @@ const Home = () => {
                     <img src={Logo} alt="Logo" className='w-38 my-3' />
                 </div>
                 <h1 className='lg:text-5xl text-xl font-semibold text-center text-blue-500'>PCCC Identity Portal</h1>
-                <p className='text-center my-2'>Hola, Admin. Please Login to Identify Club Members.</p>
+                <p className='text-center my-2'>
+                    {
+                        user ? 'Hola, Admin! You have access now. Try to do something cOOl.' : 'Hola, Admin. Please Login to Identify Club Members.'
+                    }
+                </p>
                 <div className='flex justify-center'>
                     <Link to='/login' className='btn bg-blue-500 text-white my-3'>Login</Link>
                 </div>
