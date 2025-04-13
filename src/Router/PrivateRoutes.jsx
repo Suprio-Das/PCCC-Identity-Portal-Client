6407dac081ac63cdcc0c0534fa084ba2e43837ba
Suprio-Das <priyamd507@gmail.com>
2025-04-13 23:32:30 +0600

    children return after user logged in

--- src/Router/PrivateRoutes.jsx
+++ src/Router/PrivateRoutes.jsx
@@ -2,10 +2,13 @@ import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import AuthContext from '../Context/AuthContext';
 
-const PrivateRoutes = () => {
+const PrivateRoutes = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
     if (loading) {
         return <p>Data is loading...</p>
+    }
+    if (user) {
+        return children;
     }
     return (
         <Navigate to='/login'></Navigate>
