832bc117a0009de4b5ae2903dedd329e28ecfef2
Suprio-Das <priyamd507@gmail.com>
2025-04-14 18:36:32 +0600

    Committee Members card designed

--- src/Pages/CommitteeMembers/CommitteeMembers.jsx
+++ src/Pages/CommitteeMembers/CommitteeMembers.jsx
@@ -6,7 +6,36 @@ const CommitteeMembers = () => {
     const [committeeMembers, setCommitteeMembers] = useState(loadedData);
     return (
         <div>
-            This is committee members page.
+            <div className='w-full max-w-sm rounded-xl overflow-hidden shadow-xl bg-white'>
+                <div className='profile-card h-24 relative'>
+                    <div className='absolute -bottom-12 left-1/2 transform -translate-x-1/2'>
+                        <img
+                            src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png'
+                            alt='Profile'
+                            className='w-24 h-24 rounded-full border-4 border-white bg-white'
+                        />
+                    </div>
+                </div>
+
+                <div className='mt-16 text-center px-6 pb-6'>
+                    <h2 className='text-xl font-bold text-gray-800'>Suprio Das</h2>
+                    <p className='text-sm text-gray-500'>Designation: IT Secretary</p>
+                    <p className='text-sm text-gray-500'>Batch: 28 D</p>
+                    <p className='text-sm text-gray-500'>ID: CSE 028 07546</p>
+                    <p className='text-sm text-gray-500'>Phone:
+                        01610243375
+                    </p>
+
+                    <div className='mt-6 flex gap-4 justify-center'>
+                        <button className='bg-blue-500 hover:bg-blue-600 text-white cursor-pointer px-4 py-2 rounded-lg transition'>
+                            Update
+                        </button>
+                        <button className='bg-red-500 hover:bg-red-600 text-white cursor-pointer px-4 py-2 rounded-lg transition'>
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
