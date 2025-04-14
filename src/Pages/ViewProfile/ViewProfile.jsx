11e617e5293991814d11e1c711d65c2064255bbf
Suprio-Das <priyamd507@gmail.com>
2025-04-14 14:07:50 +0600

    View profile details page creatd

---
+++ src/Pages/ViewProfile/ViewProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ViewProfile = () => {
+    return (
+        <div>
+            This is view profile page.
+        </div>
+    );
+};
+
+export default ViewProfile;
