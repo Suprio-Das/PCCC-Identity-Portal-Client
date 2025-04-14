5dd217580b90eba8fb7010bb1a3e785dadc11b09
Suprio-Das <priyamd507@gmail.com>
2025-04-14 19:16:34 +0600

    little changes

--- src/Pages/CommitteeMembers/CommitteeMembers.jsx
+++ src/Pages/CommitteeMembers/CommitteeMembers.jsx
@@ -7,7 +7,7 @@ const CommitteeMembers = () => {
     return (
         <div className='w-[90%] mx-auto my-5'>
             <h1 className='mt-3 mb-11 lg:text-3xl text-xl font-semibold text-blue-500 text-center'>Committee Members Info</h1>
-            <div className='grid lg:grid-cols-3 grid-cols-1 gap-x-5 gap-y-8 justify-items-center'>
+            <div className='grid lg:grid-cols-3 grid-cols-1 gap-x-5 gap-y-8 justify-items-center mb-11'>
                 {
                     committeeMembers.map(member => <div key={member._id} className='w-full max-w-sm rounded-xl overflow-hidden shadow-xl bg-white'>
                         <div className='profile-card h-24 relative'>
