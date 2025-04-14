6dd1b846241ecd07eaf87d4169aa9616187ebbfa
Suprio-Das <priyamd507@gmail.com>
2025-04-14 14:26:02 +0600

    matched student retrieved in view profile

--- src/Pages/ViewProfile/ViewProfile.jsx
+++ src/Pages/ViewProfile/ViewProfile.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ViewProfile = () => {
+    const { state } = useLocation();
+    const matchedStudent = state?.matchedStudent;
+    console.log(matchedStudent);
     return (
         <div>
             This is view profile page.
