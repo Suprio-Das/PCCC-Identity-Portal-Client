db1929a2a3c6dd108a6221e76bd0e196a1462fce
Suprio-Das <priyamd507@gmail.com>
2025-04-15 11:57:48 +0600

    name field added and parent div style modified

--- src/Pages/AddMember/AddMember.jsx
+++ src/Pages/AddMember/AddMember.jsx
@@ -2,8 +2,18 @@ import { MdConstruction } from "react-icons/md";
 
 const AddMember = () => {
     return (
-        <div className='min-h-[calc(100vh-130px)] flex items-center justify-center'>
-            <h1 className="lg:text-3xl font-semibold text-blue-500">Add New Club Member</h1>
+        <div className='min-h-[calc(100vh-130px)] flex flex-col items-center justify-center'>
+            <h1 className="lg:text-3xl font-semibold text-blue-500 mb-8">Add New Club Member</h1>
+            {/* Add Member Form */}
+            <div>
+                <form>
+                    {/* Name */}
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Enter Club Member Name</legend>
+                        <input type="text" className="input w-sm" name="name" placeholder="Type Club Member here" />
+                    </fieldset>
+                </form>
+            </div>
         </div>
     );
 };
