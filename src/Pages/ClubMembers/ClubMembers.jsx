b844d455f16fe9020c41c4803367e5974ebf460e
Suprio-Das <priyamd507@gmail.com>
2025-04-16 22:19:33 +0600

    Title added

--- src/Pages/ClubMembers/ClubMembers.jsx
+++ src/Pages/ClubMembers/ClubMembers.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import { BsFillClipboardDataFill } from "react-icons/bs";
 
 const ClubMembers = () => {
     return (
         <div className='w-[90%] mx-auto p-5'>
-            <h1>Club Members Page.</h1>
+            <h1 className='lg:text-2xl font-semibold text-blue-500 flex items-center justify-center gap-2'>Find Batchwise Members Data <BsFillClipboardDataFill /></h1>
         </div>
     );
 };
