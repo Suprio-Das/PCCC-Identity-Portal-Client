ab7912ef99896137537a655b0e36175d0dd43abd
Suprio-Das <priyamd507@gmail.com>
2025-04-16 22:16:11 +0600

    width set of club member component

--- src/Pages/ClubMembers/ClubMembers.jsx
+++ src/Pages/ClubMembers/ClubMembers.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ClubMembers = () => {
     return (
-        <div>
+        <div className='w-[90%] mx-auto p-5'>
             <h1>Club Members Page.</h1>
         </div>
     );
