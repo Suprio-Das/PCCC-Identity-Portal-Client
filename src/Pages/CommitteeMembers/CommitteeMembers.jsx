afb020fe080110613d3c4ac0812d9a24c4667e7d
Suprio-Das <priyamd507@gmail.com>
2025-04-24 22:02:25 +0600

    Committe member name added to the modal

--- src/Pages/CommitteeMembers/CommitteeMembers.jsx
+++ src/Pages/CommitteeMembers/CommitteeMembers.jsx
@@ -87,8 +87,7 @@ const CommitteeMembers = () => {
                             {/* Update Modal */}
                             <dialog id={`${member._id}`} className="modal modal-bottom sm:modal-middle">
                                 <div className="modal-box">
-                                    <h3 className="font-bold text-lg">Hello!</h3>
-                                    <p className="py-4">Press ESC key or click the button below to close</p>
+                                    <h3 className="font-bold text-lg">Update <span className='text-blue-500'>{member.Name}</span></h3>
                                     <div className="modal-action">
                                         <form method="dialog">
                                             {/* if there is a button in form, it will close the modal */}
